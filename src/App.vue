b7af0013463f5a620b1cbf5f39103ce58361178f
silvamateus <mateus.wtv@gmail.com>
2018-03-30 20:01:16 -0300

    Start Requisition

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
-  <template>
-  <div id="app">
-    <infinite-scroll></infinite-scroll>
-  </div>
+	<template>
+	<div id="app">
+		<infinite-scroll></infinite-scroll>
+	</div>
 </template>
 
 <script>
 import InfiniteScroll from './components/InfiniteScroll.vue'
 
 export default {
-  name: 'app',
-  components: {
-    InfiniteScroll
-  }
+	data(){
+		return{
+			posts: []
+		}
+	},
+	name: 'app',
+	components: {
+		InfiniteScroll
+	},
+	methods:{
+		getData: function getData(){
+			fetch('http://www.mocky.io/v2/5abea2d12e00004d006f7e35')
+				.then(res => res.json())
+				.then(res => this.posts = this.posts.concat(res.posts))
+		}
+	}
 }
 </script>
 
 <style lang="scss">
-  body{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  #app{
-    padding-top: 1em;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-  }
+	*{
+		box-sizing: border-box;
+	}
+
+	body{
+		margin: 0;
+		padding: 0;
+	}
+
+	#app{
+		padding-top: 1em;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+	}
 </style>
