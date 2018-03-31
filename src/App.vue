a70e5456c5eb10c74a79fc5b3bfea980c9c0272a
silvamateus <mateus.wtv@gmail.com>
2018-03-31 09:26:39 -0300

    Spacing components

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,56 @@
-	<template>
-	<div id="app">
-		<infinite-scroll></infinite-scroll>
-	</div>
+<template>
+  <div id="app">
+    <infinite-scroll v-for="(post, ind) in posts" :key="ind" :post="post"></infinite-scroll>
+    <loading v-if="isLoading"></loading>
+  </div>
 </template>
 
 <script>
 import InfiniteScroll from './components/InfiniteScroll.vue'
+import Loading from './components/Loading.vue'
 
 export default {
-	data(){
-		return{
-			posts: []
-		}
-	},
-	name: 'app',
-	components: {
-		InfiniteScroll
-	},
-	methods:{
-		getData: function getData(){
-			fetch('http://www.mocky.io/v2/5abea2d12e00004d006f7e35')
-				.then(res => res.json())
-				.then(res => this.posts = this.posts.concat(res.posts))
-		}
-	}
+  data() {
+    return {
+      isLoading: false,
+      posts: []
+    }
+  },
+  name: 'app',
+  components: {
+    InfiniteScroll,
+    Loading
+  },
+  methods: {
+    getData: function() {
+      this.isLoading = true
+      fetch('http://www.mocky.io/v2/5abed14c2e000064006f7e59')
+        .then(res => res.json())
+        .then(res => (this.posts = this.posts.concat(res.posts)))
+        .finally(this.isLoading = false)
+    }
+  },
+  mounted() {
+    this.getData()
+  }
 }
 </script>
 
 <style lang="scss">
-	*{
-		box-sizing: border-box;
-	}
+* {
+  box-sizing: border-box;
+}
 
-	body{
-		margin: 0;
-		padding: 0;
-	}
+body {
+  margin: 0;
+  padding: 0;
+}
 
-	#app{
-		padding-top: 1em;
-		width: 100%;
-		display: flex;
-		justify-content: center;
-	}
+#app {
+  padding-top: 1em;
+  padding-left: 5em;
+  width: 93%;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
